7cb2f2a6689f11f907785ea94f44dcba3d43ef2f
HectorARG <hectorr9577@gmail.com>
2024-02-07 14:33:11 -0600

    Creacion de origenes de datos y Repositorios

---
+++ src/domain/datasource/auth.datasource.ts
@@ -0,0 +1,15 @@
+
+
+import { UserEntity } from "../entities/user.entity";
+import { RegisterUserDTO } from '../dtos/auth/register-user.dto';
+
+//La clase es abstracta porque no voy a crear instancias de esta clase 
+// Solo servira para definir reglas
+export abstract class AuthDatasource {
+
+    //todo: Tarea hacer el login
+
+    //
+    abstract register( registerUserDTO: RegisterUserDTO ):Promise<UserEntity>
+
+}
